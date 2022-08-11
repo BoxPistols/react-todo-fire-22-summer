6aed0016f42d20945523cce28fb80db8f51bba9e
BoxPistols <ito.atsu.mail@gmail.com>
2022-08-12 06:20:45 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { useState } from 'react';
+import 'asagiri';
 
 import './style.css';
 
 export default function App() {
+  // input
+  const [input, setInput] = useState('');
+  // list
+  const [todoList, setTodoList] = useState([]);
+
+  // add todo
+  const addTodo = () => {
+    setTodoList([...todoList, input]);
+    setInput('');
+  };
+  console.log(input); // input text
+
+  const deleteTodo = (index) => {
+    setTodoList(todoList.filter((_, idx) => idx !== index));
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <selection className="section">
+        <input
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <button onClick={() => addTodo()}>追加</button>
+
+        {/* <Todo todoList={todoList} deleteTodo={deleteTodo} /> */}
+      </selection>
     </div>
   );
 }
